3c42fce22d8502f89b8b09773eefb70f22f02b54
amendoza514 <amendoza15@cmc.edu>
2021-01-05 15:50:24 -0800

    comments

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+// express framework for leveraging node
 import bodyParser from 'body-parser';
+//body parser for post requests
 import mongoose from 'mongoose';
+//mongoose for creating models to use in conjunction with Mongo
 import cors from 'cors';
+//handle cross origin requests
 
 // ^ newest version of node can use this syntax instead of const express = require('express')
 // ^ for reference I needed to go into package.json and add the following line: "type": "module",
@@ -12,5 +16,5 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-//mongoDB cluster
+//mongoDB cluster added here
 
