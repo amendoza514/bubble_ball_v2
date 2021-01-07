c429e37188c29074e19d77cb4832d68412d832e4
amendoza514 <amendoza15@cmc.edu>
2021-01-06 17:03:43 -0800

    add model to postScore

--- server/controllers/scores.js
+++ server/controllers/scores.js
@@ -1,9 +1,26 @@
 //handlers for our routes
+import PostScore from '../models/postScore.js';
 
-export const getScores = (req, res) => {
-    res.send('This works!')
+export const getScores = async (req, res) => {
+    try {
+        const postScores = await PostScore.find();
+        console.log(postScores);
+
+        res.status(200).json(postScores);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export const createScore = (req, res) => {
-    res.send('Score Creation')
+export const createScore = async (req, res) => {
+    const score = req.body;
+    const newScore = new  PostScore(score);
+
+    try {
+        await newScore.save();
+
+        res.status(201).json(newScore);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
