47c205a27d8b7bc8a548d29c15dc63692693ef73
amendoza514 <amendoza15@cmc.edu>
2021-01-14 14:38:01 -0800

    delete demo functionality

--- server/controllers/stats.js
+++ server/controllers/stats.js
@@ -1,3 +1,4 @@
+import  mongoose  from 'mongoose';
 import unirest from 'unirest';
 import Stat from '../models/Stat.js';
 
@@ -10,28 +11,42 @@ export const getStats = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-   
 };
 
 export const createStat = async (req, res) => {
     var req = unirest("GET", "https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/265");
 
     req.headers({
-	"x-rapidapi-key": "c58da8187bmsh4f7d4d00df2f25dp126899jsn3c9c7b80e407",
-	"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-	"useQueryString": true
+      "x-rapidapi-key": "c58da8187bmsh4f7d4d00df2f25dp126899jsn3c9c7b80e407",
+      "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+      "useQueryString": true
     });
 
-  req.end(function (res) {
-    if (res.error) console.log('nope');
-    let randInt =   Math.floor(Math.random() * Math.floor(300));
-    let data = res.body.api.statistics[randInt]
-    console.log(data.points)
-    console.log(data.assists)
-    const newStat = new Stat({ name:"lebron",scoreAvg:data.points, assistAvg:data.assists, rebAvg:data.totReb });
-    newStat.save();
-  });
-
-    // const { name, scoreAvg, assistAvg, rebAvg } = req.body;
-    // const newStat = new Stat({ name, scoreAvg, assistAvg, rebAvg });
+    req.end(function (res) {
+      if (res.error) console.log('nope');
+      let randInt =   Math.floor(Math.random() * Math.floor(300));
+      let data = res.body.api.statistics[randInt]
+      // console.log(data.points)
+      const newStat = new Stat({ name:"lebron",scoreAvg:data.points, assistAvg:data.assists, rebAvg:data.totReb });
+      newStat.save();
+    });
 };
+
+export const updateStat = async (req, res) => {
+      const { id: _id } = req.params;
+      const stat = req.body
+
+      if (mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('no stat that matches');
+
+      const updatedStat = await Stat.findByIdAndUpdate(_id, stat, { new: true});
+
+      res.json(updatedStat);
+}
+
+export const deleteStat = async (req,res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('no stat that matches');
+    await Stat.findByIdAndRemove(id);
+
+    res.json({ message: 'Stat deleted successfully'})
+}
--- server/routes/stats.js
+++ server/routes/stats.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getStats, createStat } from '../controllers/stats.js';
+import { getStats, createStat, updateStat, deleteStat } from '../controllers/stats.js';
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.get('/', getStats);
 router.post('/', createStat);
+router.patch('/:id', updateStat);
+router.delete('/:id', deleteStat);
 
 export default router;
